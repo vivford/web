02b3d4d2770156b0c35db9963e5a7863a706aabf
nya1 <nya1@protonmail.com>
2016-06-29 19:35:11 +0200

    new website

--- pull.php
+++ pull.php
@@ -1,3 +1,13 @@
-<pre>
 <?php
-passthru("git pull");
+error_reporting(0);
+if(empty($_GET["s"])) return;
+$s = (string) htmlentities($_GET["s"], ENT_QUOTES, "UTF-8");
+if(strlen($s)>=50 || $s=='') return;
+
+$hashed = hash('sha256',hash('sha256',$s));
+
+if($hashed=='49d09a923d5fc604b77c7a08d9a30d70daa7a558b8a05f8c4e62ec69d44a7db5'){
+	passthru("git pull");
+} else {
+	return;
+}
