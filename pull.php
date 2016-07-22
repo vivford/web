4798a035e9aa9575d225071aed6a5567d7cab376
George Hotz <george@comma.ai>
2016-07-22 07:51:27 -0700

    revert

--- pull.php
+++ pull.php
@@ -1,13 +1,6 @@
+<pre>
 <?php
-error_reporting(0);
-if(empty($_GET["s"])) return;
-$s = (string) htmlentities($_GET["s"], ENT_QUOTES, "UTF-8");
-if(strlen($s)>=50 || $s=='') return;
-
-$hashed = hash('sha256',hash('sha256',$s));
-
-if($hashed=='49d09a923d5fc604b77c7a08d9a30d70daa7a558b8a05f8c4e62ec69d44a7db5'){
-	passthru("git pull");
-} else {
-	return;
+if (hash('sha1', $_GET['pw']) == '895f00452bb02cb5c394b42f7f2323448a22014e') {
+  passthru("git pull");
 }
+
